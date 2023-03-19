f158d4821add2fcac7aac78174a95515ad44c774
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-03-19 13:27:05 +0300

    Code Clean

---
+++ src/app/Layouts/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>Project working</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
